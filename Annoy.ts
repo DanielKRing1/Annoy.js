f41d7a68a68246b63f07d8f24b5d36a661569ee4
DanielKRing1 <DanielKRing1@gmail.com>
2021-04-29 00:08:08 -0700

    Init git ignore + Add npm @types to package.json

--- Annoy.ts
+++ Annoy.ts
@@ -38,30 +38,3 @@ export default class Annoy {
         }
     }
 }
-
-// TEST
-
-const POINT_COUNT = 10000;
-const VECTOR_LEN = 128;
-const K = 500;
-
-const a: Annoy = new Annoy(10, 50);
-
-console.time('Annoy setup');
-// 1. Generate random points
-const points: Vector[] = [];
-for (let i = 0; i < POINT_COUNT; i++) {
-    const p: Vector = [...new Array(VECTOR_LEN)].map(() => Math.random() * 40);
-    a.add(p);
-}
-console.timeEnd('Annoy setup');
-
-// 3. Get KNN
-console.time('Annoy KNN');
-const knn: Vector[] = a.get(
-    [...new Array(VECTOR_LEN)].map(() => Math.random() * 40),
-    K
-);
-console.timeEnd('Annoy KNN');
-
-console.log(knn.length);
